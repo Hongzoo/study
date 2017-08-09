136a8cc0ae67b2a7ca75ebd41b25bb5dda93ba1b
Hongzoo <wow_ac@naver.com>
2017-08-09 11:17:26 +0900

    5주차 과제 플러그인화

--- JavaScript/se_ui/exam05/js/exam.View.tab.js
+++ JavaScript/se_ui/exam05/js/exam.View.tab.js
@@ -9,72 +9,95 @@
 var exam = exam || {};
 exam.View = exam.View || {};
 
-exam.View.tab = function() {
+exam.View.mySlider = function(opts) {
+	var defParams = {
+		wrap: '.cast_box',
+		btnArea: '.cast_tab',
+		contArea: '.cast_cont',
+		controlBtnArea: '.cast_btn',
+		pageNum: false // 'page 번호 없음'이 기본세팅 값
+	}
+	this.opts = $.extend({}, defParams, opts); // opts 값 받아온 것 중 겹치는 키값 있으면 덮어쓰고, 겹치는 키값 없으면 기본 defParams 값으로 사용
 	this.init();
 };
 
-exam.View.tab.prototype = {
-	_welClickBtn: null,
+exam.View.mySlider.prototype = {
+	_nIndex: 0,
+	_welCurrentBtn: null, // 선택된(current) paging 버튼 저장
 	init: function() {
 		this._assignElements();
 		this._attachEventHandlers();
-		this._initLayout();
+		this._initLayout(); // 도큐먼트 다 읽은 후 처음 1번 세팅 할 내용들
 	},
 	_assignElements: function() {
-		this.welTabWrap = $('.cast_box');
-		this.welTabArea = this.welTabWrap.find('.cast_tab');
-		this.welTabItem = this.welTabArea.find('li');
-		this.welTabBtn = this.welTabItem.find('a');
-		this.welTabCont = this.welTabWrap.find('.cast_cont');
-		this.welTabPrevBtn = this.welTabWrap.find('.cast_btn button').eq(0);
-		this.welTabNextBtn = this.welTabWrap.find('.cast_btn button').eq(1);
-		this.welTabPageArea = this.welTabWrap.find('.cast_num');
-		this.welTabCurrentPage = this.welTabPageArea.find('.current');
-		this.welTabTotalPage = this.welTabPageArea.find('.total');
+		this._welTabWrap = $(this.opts.wrap); // 플러그인(?) 호출시 옵션으로 받은 것이 있으면 사용하고, 받은 것이 없다면 기본값(defParams) 사용
+		this._welTabArea = this._welTabWrap.find(this.opts.btnArea); // 플러그인(?) 호출시 옵션으로 받은 것이 있으면 사용하고, 받은 것이 없다면 기본값(defParams) 사용
+		this._welTabItem = this._welTabArea.find('li');
+		this._welTabBtn = this._welTabItem.find('a');
+		this._welTabCont = this._welTabWrap.find(this.opts.contArea); // 플러그인(?) 호출시 옵션으로 받은 것이 있으면 사용하고, 받은 것이 없다면 기본값(defParams) 사용
+		this._welControlBtns = this._welTabWrap.find(this.opts.controlBtnArea); // 플러그인(?) 호출시 옵션으로 받은 것이 있으면 사용하고, 받은 것이 없다면 기본값(defParams) 사용
+		this._welTabPrevBtn = this._welControlBtns.find('button').eq(0);
+		this._welTabNextBtn = this._welControlBtns.find('button').eq(1);
+
+		if(this.opts.pageNum == true) { // 플러그인(?) 호출시 'pageNum: true' 로 옵션값을 넣어준 경우만 실행. pageNum 관련 추가 세팅
+			this._welPageNumArea = this._welTabWrap.find(this.opts.pageNumArea);
+			this._welCurrentPageNum = this._welPageNumArea.find('.current');
+			this._welTotalPageNum = this._welPageNumArea.find('.total');
+		}
 	},
 	_attachEventHandlers: function() {
-		var oself = this;
-		this.welTabBtn.on('click', function(e) {
-			oself._welClickBtn = $(this);
-			oself._onClickTabBtn(e);
-		});
-		this.welTabPrevBtn.on('click', $.proxy(this._onClickPrevBtn, this));
-		this.welTabNextBtn.on('click', $.proxy(this._onClickNextBtn, this));
+		this._welTabBtn.on('click', $.proxy(this._onClickTabBtn, this));
+		this._welTabPrevBtn.on('click', $.proxy(this._onClickPrevBtn, this));
+		this._welTabNextBtn.on('click', $.proxy(this._onClickNextBtn, this));
 	},
 	_initLayout : function() {
-		this.nIndex = this.welTabArea.find('li.active').index();
-		this.welTabTotalPage.text(this.welTabItem.length);
+		this._nIndex = this._welTabArea.find('li.active').index();
+		if(this.opts.pageNum == true) {
+			this._welTotalPageNum.text(this._welTabItem.length);
+		}
 	},
 	_onClickTabBtn: function(e) {
 		e.preventDefault();
-		this._activeTab(this._welClickBtn);
+		this._welCurrentBtn = $(e.currentTarget);
+		this._activeTab(this._welCurrentBtn);
 	},
 	_onClickPrevBtn: function() {
-		if(this.nIndex == 0) {
-			this._activeTab(this.welTabItem.eq(this.welTabItem.length-1).find('a'));
+		if(this._nIndex == 0) {
+			this._activeTab(this._welTabItem.eq(this._welTabItem.length-1).find('a'));
 			return false;
 		}
-		this._activeTab(this.welTabItem.eq(this.nIndex-1).find('a'));
+		this._activeTab(this._welTabItem.eq(this._nIndex-1).find('a'));
 	},
 	_onClickNextBtn: function() {
-		if(this.nIndex == (this.welTabItem.length-1)) {
-			this._activeTab(this.welTabItem.eq(0).find('a'));
+		if(this._nIndex == (this._welTabItem.length-1)) {
+			this._activeTab(this._welTabItem.eq(0).find('a'));
 			return false;
 		}
-		this._activeTab(this.welTabItem.eq(this.nIndex+1).find('a'));
+		this._activeTab(this._welTabItem.eq(this._nIndex+1).find('a'));
 	},
 	_activeTab: function(tabBtn) {
 		var targetCont = tabBtn.attr('href');
-		this.nIndex = tabBtn.parent().index();
+		this._nIndex = tabBtn.parent().index();
 		tabBtn.parent().addClass('active').siblings().removeClass('active');
-		this.welTabCont.find(targetCont).addClass('active').siblings().removeClass('active');
-		this._refreshPageNum();
+		this._welTabCont.find(targetCont).addClass('active').siblings().removeClass('active');
+		if(this.opts.pageNum == true) {
+			this._refreshPageNum();
+		}
 	},
 	_refreshPageNum: function() {
-		this.welTabCurrentPage.text(this.nIndex+1);
+		this._welCurrentPageNum.text(this._nIndex+1);
 	}
 };
 
 $(function() {
-	var oMyTab = new exam.View.tab();
+	var oMyTab = new exam.View.mySlider({
+		pageNum: true,
+		pageNumArea: '.cast_num'
+	});
+	var oMySlider = new exam.View.mySlider({
+		wrap: '.slide_box',
+		btnArea: '.slide_tab',
+		contArea: '.slide_cont',
+		controlBtnArea: '.slide_btn'
+	});
 })
